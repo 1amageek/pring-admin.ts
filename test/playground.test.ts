7a8d11c3824ffc7523add692169d5c42d4bdbe1b
1amageek <tmy0x3@icloud.com>
2019-02-06 18:43:45 +0900

    fix

---
+++ test/playground.test.ts
@@ -0,0 +1,31 @@
+process.env.NODE_ENV = 'test';
+import * as admin from 'firebase-admin'
+import * as Pring from "../src/index"
+
+var key = require("../key.json")
+const app = admin.initializeApp({
+    credential: admin.credential.cert(key)
+})
+
+
+Pring.initialize(admin.firestore())
+
+import { Document } from './document'
+import { request } from "http";
+
+const main = async () => {
+
+	const document: Document = new Document()
+	document.string = "aaa"
+	await document.save()
+
+	const doc: Document = await new Document(document.id, {}).fetch()
+	console.log(doc.updateValue())
+}
+
+// Playground
+describe("Playground", async () => {
+    test("main", async () => {
+        await main()
+    })
+})
